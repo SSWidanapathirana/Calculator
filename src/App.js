4bf97af6dd82a8c9f0e5e5a622944935c127629e
SSWidanapathirana <84173987+SSWidanapathirana@users.noreply.github.com>
2022-09-03 19:22:51 +0530

    commit changes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import calculator from './component/calculator/calculator';
+
+function App() {
+  return (
+    <div>
+      <calculator/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/calculator/calculator.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './calculator.css';
+
+const calculator = () => {
+  return (
+    <div>calculator</div>
+  )
+}
+
+export default calculator
